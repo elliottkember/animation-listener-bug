dcfab62bddfa1c0916a26127c4f45b31d4dc50a4
Elliott Kember <elliott.kember@gmail.com>
2024-03-21 16:29:10 +1300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, Button, Text, View } from 'react-native';
 
 export default function App() {
+  const [key, setKey] = useState('initial');
+  const animatedValue = useRef(new Animated.Value(1)).current;
+  const initialized = useRef(false);
+  const [displayedValue, setDisplayedValue] = useState(1);
+
+  if (!initialized.current) {
+    animatedValue.addListener(({ value: v }) => {
+      setDisplayedValue(v);
+    });
+    initialized.current = true;
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      animatedValue.setValue(Math.random());
+    }, 500);
+    return () => clearInterval(interval);
+  }, [animatedValue]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ margin: 32, marginVertical: 64 }}>
+      <Text />
+      {key === 'initial' ? (
+        <Text>This value updates:</Text>
+      ) : (
+        <Text>
+          This value{' '}
+          <Text style={{ fontWeight: 'bold' }}>no longer updates</Text>:
+        </Text>
+      )}
+
+      <Text>{displayedValue}</Text>
+      <Text />
+
+      <Text>This square changes size:</Text>
+      <Animated.View
+        key={key.toString()}
+        style={{
+          transform: [{ scale: animatedValue }],
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+        }}
+      />
+
+      <Button title="Remount animation" onPress={() => setKey('remounted')} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
